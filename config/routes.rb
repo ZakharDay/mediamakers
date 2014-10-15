f016616d4fd66eaf6f498dc5fdee6a9eede05af1
ZakharDay <zakhar.day@gmail.com>
2014-10-15 16:21:54 +0400

    Route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # resources :landings, only: :index
+  resources :landings, only: :index
   root 'landings#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
