1131a9bfd5ad24595efca04fd72c22a5b26e172c
ZakharDay <zakhar.day@gmail.com>
2014-10-16 11:23:19 +0400

    Fixes

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,4 +1,10 @@
 class LandingsController < ApplicationController
   def index
+    @title = 'MediaMakers — 29 октября в DI Telegraph'
+    @about =  "Первая конференция о людях, меняющих
+              ландшафт медиа. Людях, создающих
+              инновационные проекты и продукты.
+              Людях, переворачивающих привычную
+              среду с ног на голову. Здесь и сейчас"
   end
 end
