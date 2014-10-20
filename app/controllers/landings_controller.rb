14ae4acfc8de10a68b126e8f5b35bd2520c3954d
ZakharDay <zakhar.day@gmail.com>
2014-10-20 16:21:52 +0400

    Share test

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,6 +1,7 @@
 class LandingsController < ApplicationController
   def index
     @title = 'MediaMakers — 29 октября в DI Telegraph'
+    @description = 'Slon и РВК представляют MediaMakers — первую конференцию о людях, меняющих ландшафт медиа. 29 октября в DI Telegraph.'
     # @about =  ""
   end
 end
