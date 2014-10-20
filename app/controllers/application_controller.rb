61a6d2394172dbdbca2cea84cc57fd7155bb4b36
ZakharDay <zakhar.day@gmail.com>
2014-10-20 08:01:26 +0400

    Some features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper :all
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS/
+  end
+  helper_method :mobile_device?
 end
